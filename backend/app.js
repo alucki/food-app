b4aa9843a8a9df00fe309a23da1714f3f62a3389
Anna Lucking <alucking72@gmail.com>
2021-01-08 16:17:01 +0000

    CORS and bodyParser middleware

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,18 @@
 // project configurations
 const express = require("express");
+const bodyParser = require("body-parser");
+const routes = require("./routes/postsRoutes");
+
 const app = express(); // initialise app
 
-// import routes
-const routes = require("./routes/postsRoutes");
+// CORS: allows data to be requested from another domain
+// outside the domain from which the data is being served
+app.use((req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+});
+
+// middleware to parse request bodies to JSON
+app.use(bodyParser.json());
 
 // middleware to use our routes
 app.use("/", routes);

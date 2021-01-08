4762ec2a6da0057a2ac525c3676d83b87a698662
Anna Lucking <alucking72@gmail.com>
2021-01-08 16:36:17 +0000

    use cors package

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const routes = require("./routes/postsRoutes");
+const cors = require("cors");
 
 const app = express(); // initialise app
 
 // CORS: allows data to be requested from another domain
 // outside the domain from which the data is being served
-app.use((req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-});
+app.use(cors());
 
 // middleware to parse request bodies to JSON
 app.use(bodyParser.json());

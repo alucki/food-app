363c0857b52700557d389bd31c503c4600503bcb
Anna Lucking <alucking72@gmail.com>
2021-01-15 13:57:30 +0000

    use morgan for HTTP logging

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const routes = require("./routes/postsRoutes");
 const cors = require("cors");
+const morgan = require("morgan");
+
+// In Node.js and Express, middleware is a function
+// that has access to the request and response lifecycle methods,
+// and the next() method to continue logic in your Express server.
 
 const app = express(); // initialise app
 
@@ -12,6 +17,10 @@ app.use(cors());
 
 // middleware to parse request bodies to JSON
 app.use(bodyParser.json());
+
+// morgan's built-in method, identify the URL,
+// declare a status, and the request’s response time in milliseconds
+app.use(morgan("tiny"));
 
 // middleware to use our routes
 app.use("/", routes);

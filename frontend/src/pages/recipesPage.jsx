b77fa71027f0d47a266354f9019f2dd0ed153d52
Anna Lucking <alucking72@gmail.com>
2021-01-22 15:08:09 +0000

    get all recipes on recipes page

--- frontend/src/pages/recipesPage.jsx
+++ frontend/src/pages/recipesPage.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
-// import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const RecipesPage = () => {
+  const [recipes, setRecipes] = useState();
+
+  useEffect(() => {
+    const getRecipes = async () => {
+      const recipes = await axios.get(
+        'http://localhost:3000/getPosts',
+      );
+      setRecipes(recipes.data);
+    };
+
+    getRecipes();
+  }, []);
+
   return <div>Recipes page</div>;
 };
 
